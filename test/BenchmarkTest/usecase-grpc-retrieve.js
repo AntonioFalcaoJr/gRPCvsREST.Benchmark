2377189ec21d7c3f8df58a70a675b51e11419924
Kauan Schumacher <kauan.schumacher@xpi.com.br>
2022-07-14 16:39:53 -0300

    Fix

--- test/BenchmarkTest/usecase-grpc-retrieve.js
+++ test/BenchmarkTest/usecase-grpc-retrieve.js
@@ -1,7 +1,15 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
 
 export default function () {
   http.get('https://test.k6.io');
+
+  check
+
   sleep(1);
 }
--- test/BenchmarkTest/usecase-rest-submission.js
+++ test/BenchmarkTest/usecase-rest-submission.js
@@ -1,7 +1,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 10,
+  duration: '30s',
+}; 
+
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
-}s
+  let res = http.get('https://test.k6.io');
+  check(res, { 'status was 200': (r) => r.status == 200 })
+}
