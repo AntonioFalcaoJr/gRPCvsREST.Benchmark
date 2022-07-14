2a1a320f0c216d1d2822aa4e29f5f4dc2f474bb1
Kauan Schumacher <kauan.schumacher@xpi.com.br>
2022-07-14 16:56:26 -0300

    Create BFF endpoints

--- src/Web/BFF.WebAPI/Program.cs
+++ src/Web/BFF.WebAPI/Program.cs
@@ -1,25 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.MapGet("/grpc/retrieve", () => { });
+app.MapPost("/grpc/submit", () => { });
+
+app.MapGet("/rest/retrieve", () => { });
+app.MapPost("/rest/submit", () => { });
+
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
