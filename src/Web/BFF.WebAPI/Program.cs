b53744a62b26f03524d9e0384be226a3d9c5698c
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-09-01 12:43:30 -0300

    new tests

--- src/Web/BFF.WebAPI/Program.cs
+++ src/Web/BFF.WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using System.IO.Compression;
 using BFF.WebAPI;
 using BFF.WebAPI.HttpClients;
+using Grpc.Core;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Balancer;
+using Grpc.Net.Client.Configuration;
 using GRPCvsREST.Benchmark;
 using Microsoft.AspNetCore.HttpLogging;
 using Serilog;
@@ -25,6 +29,12 @@ builder.Services
     .AddEndpointsApiExplorer()
     .AddSwaggerGen(options => options.CustomSchemaIds(type => type.FullName));
 
+// SETUP - 120K
+// builder.Services.AddGrpcClient<BenchmarkService.BenchmarkServiceClient>(options 
+//     => options.Address = new(builder.Configuration["Benchmark:GrpcClient"]!));
+
+//AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
 // SETUP - 273k request/min (1BFF = 1Server)
 // builder.Services.AddSingleton(GrpcChannel.ForAddress(builder.Configuration["Benchmark:GrpcClient"]!,
 //     new GrpcChannelOptions {HttpHandler = new SocketsHttpHandler {EnableMultipleHttp2Connections = true}}));
@@ -36,24 +46,78 @@ builder.Services
 //     .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler { EnableMultipleHttp2Connections = true });
 
 // SETUP - 310k request/min (1BFF = 1Server)
-builder.Services.AddSingleton(GrpcChannel.ForAddress(builder.Configuration["Benchmark:GrpcClient"]!,
-    new GrpcChannelOptions
+
+builder.Services.AddSingleton<ResolverFactory>(
+    sp => new DnsResolverFactory(TimeSpan.FromSeconds(30)));
+
+builder.Services.AddSingleton(provider => GrpcChannel.ForAddress(builder.Configuration["Benchmark:GrpcClient"]!,
+    new ()
     {
+        Credentials = ChannelCredentials.Insecure,
+
         HttpHandler = new SocketsHttpHandler
         {
             EnableMultipleHttp2Connections = true,
+            
             PooledConnectionLifetime = Timeout.InfiniteTimeSpan,
             PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+            
+            KeepAlivePingDelay = TimeSpan.FromSeconds(60),
+            KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+        },
 
-            KeepAlivePingDelay = TimeSpan.FromSeconds(30),
-            KeepAlivePingTimeout = TimeSpan.FromSeconds(10)
-        }
+        ServiceConfig = new()
+        {
+            LoadBalancingConfigs =
+            {
+                new RoundRobinConfig()
+            }
+        },
+
+        ServiceProvider = provider
     }));
 
 builder.Services.AddScoped(p => new BenchmarkService.BenchmarkServiceClient(p.GetRequiredService<GrpcChannel>()));
 
 // SETUP - 298k request/min (1BFF = 1Server)
-// builder.Services.AddGrpcClientMultiplexed<BenchmarkService.BenchmarkServiceClient>();
+//builder.Services.AddGrpcClientMultiplexed<BenchmarkService.BenchmarkServiceClient>();
+
+//builder.Services.AddSingleton<ResolverFactory>(provider => new DnsResolverFactory(TimeSpan.FromSeconds(30)));
+
+// // SETUP
+// builder.Services.AddGrpcClient<BenchmarkService.BenchmarkServiceClient>(options =>
+//     {
+//         options.Address = new(builder.Configuration["Benchmark:GrpcClient"]!);
+//     })
+//     .ConfigureChannel(options =>
+//         {
+//             options.Credentials = ChannelCredentials.Insecure;
+//
+//             options.ServiceConfig = new()
+//             {
+//                 LoadBalancingConfigs =
+//                 {
+//                     new RoundRobinConfig()
+//                 }
+//             };
+//         }
+//     )
+//     .ConfigurePrimaryHttpMessageHandler(() =>
+//     {
+//         return new SocketsHttpHandler
+//         {
+//             EnableMultipleHttp2Connections = true,
+//
+//             PooledConnectionLifetime = Timeout.InfiniteTimeSpan,
+//             PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+//
+//             KeepAlivePingDelay = TimeSpan.FromSeconds(30),
+//             KeepAlivePingTimeout = TimeSpan.FromSeconds(10)
+//         };
+//     })
+//     .EnableCallContextPropagation(options
+//         => options.SuppressContextNotFoundErrors = true);
+
 
 builder.Services.AddHttpClient<IRestHttpClient, RestHttpClient>(client =>
         client.BaseAddress = new(builder.Configuration["Benchmark:RestClient"]!))
@@ -74,7 +138,7 @@ app.MapGet("/grpc/health", ([AsParameters] Requests.GrpcHealthRequest request)
     => request.Client.HealthAsync(new()).ResponseAsync);
 
 app.MapGet("/grpc", ([AsParameters] Requests.GrpcRetrieveRequest request)
-    => request.Client.RetrieveAsync(new() {Amount = request.Amount}).ResponseAsync);
+    => request.Client.RetrieveAsync(new() { Amount = request.Amount }).ResponseAsync);
 
 app.MapPost("/grpc", ([AsParameters] Requests.GrpcSubmitRequest request)
     => request.Client.SubmitAsync(new()).ResponseAsync);
